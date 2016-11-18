6a14d925f54de8b0de2dd28326631ed27e9b77b3
xingyaowang <xingyao.wang@utexas.edu>
2016-11-18 17:19:09 -0600

    source input

--- wave.cpp
+++ wave.cpp
@@ -19,7 +19,7 @@ int main() {
     {
     	for(int i  = 0; i < n; i++)
     	{
-			arr[i] = sin(10*i*cycle/n);
+			arr[i] = abs(sin(10*i*cycle/n));
     		cout << arr[i] << endl;
     	}
 	}
@@ -31,14 +31,20 @@ int main() {
 		arr[1] = 4.0/n;
 		for(int i = 1; i < n - 1; i++)
 			{
-				if(arr[i] < 1 && arr[i] > -1 && arr[i] > arr[i-1])
+				if(arr[i] < 1 && arr[i] > 0 && arr[i] > arr[i-1])
 					arr[i+1] = arr[i]+40.0/n;
 				else if(arr[i] >= 1)
+				{
+					arr[i] = 1;
 					arr[i+1] = arr[i]-40.0/n;
-				else if(arr[i] < 1 && arr[i] > -1 && arr[i] < arr[i-1])
+				}
+				else if(arr[i] < 1 && arr[i] > 0 && arr[i] < arr[i-1])
 					arr[i+1] = arr[i]-40.0/n;
-				else if(arr[i] <= -1.0)
+				else if(arr[i] <= 0)
+				{
+					arr[i] = 0;
 					arr[i+1] = arr[i]+40.0/n;
+				}
 				else
 					arr[i+1] = 500;
 
@@ -47,17 +53,17 @@ int main() {
 	}
 
 	// rectangle wave
-	else if(input ==3)
-	{
-		for(int i = 1; i < n; i++)
-		{
-			if((20*i/n) % 2 == 0)
-				arr[i] = 1;
-			else
-				arr[i] = -1;
-			cout << arr[i] << endl;
-		}
-	}
+	// else if(input ==3)
+	// {
+	// 	for(int i = 1; i < n; i++)
+	// 	{
+	// 		if((20*i/n) % 2 == 0)
+	// 			arr[i] = 1;
+	// 		else
+	// 			arr[i] = -1;
+	// 		cout << arr[i] << endl;
+	// 	}
+	// }
 	else
 		cout << "wrong input"<< endl;
 
