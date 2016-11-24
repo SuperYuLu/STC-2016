88c7697b439ed1c2f97a78c22aee345945dd592d
Yu Lu <physicsluyu@gmail.com>
2016-11-24 16:12:06 -0600

    modified main.h

---
+++ src/main.h
@@ -0,0 +1,10 @@
+//main.h
+#ifndef __MAIN_H_INCLUDED__
+#define __MAIN_H_INCLUDED__
+
+double calCurrent(string type, double t);
+double calField(double currI, double pos);
+double findFieldMin(double tField[]);
+double findFieldMax(double tField[]);
+
+
