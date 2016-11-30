8ecd0cd8a1e340cb61d7effdb1dc5bbcf44bef9a
Yu Lu <physicsluyu@gmail.com>
2016-11-30 01:04:30 -0600

    add python to plot

--- src/cleanField.cc
+++
@@ -1,8 +0,0 @@
-// cleanField.cc
-#include "traps.h"
-#include "const.h"
-void traps :: cleanField(){
-  for(int i = 0; i < zStepNum; i++){
-    tzField[i][1] = 0.0;
-  }
-}
--- src/cleanTrapsField.cc
+++
@@ -1,9 +0,0 @@
-// cleanTrapsField.cc
-#include "traps.h"
-#include "const.h"
-void cleanTrapsField(traps tp[]){
-  for(int i = 0; i < totTraps; i++){
-    tp[i].cleanField();
-  }
-}
-
